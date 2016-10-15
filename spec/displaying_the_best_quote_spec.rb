3885ef9deb683e378a4846d1e5c217d4fab59f6b
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-10-15 20:46:51 +0100

    Fake it 'til we make it - when a best quote is found, its monthly repayment is displayed to 2 d.p.

--- spec/displaying_the_best_quote_spec.rb
+++ spec/displaying_the_best_quote_spec.rb
@@ -1,6 +1,21 @@
+# coding: utf-8
 describe 'Displaying the best quote' do
   describe 'when a best quote is found' do
-    it 'displays the monthly repayment amount to 2 d.p.'
+    def display_best_quote(amount, markets)
+      'Monthly repayment: £30.78'
+    end
+    
+    it 'displays the monthly repayment amount to 2 d.p.' do
+      loan = 1000
+      markets = double(:markets_csv)
+      allow(markets).
+        to(receive(:best_quote).with(loan).
+            and_return(monthly_repayment: '£30.78'))
+      
+      best_quote = display_best_quote(loan, markets)
+      
+      expect(best_quote).to include 'Monthly repayment: £30.78'
+    end
 
     it 'displays the total replayment amount to 2 d.p.'
 
