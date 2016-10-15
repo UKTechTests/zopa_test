dcc37eb9f035e255b4e2033b18f5f76f622db2bc
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-10-15 20:07:57 +0100

    Test list - repayment amounts should be displayed to 2 d.p.

---
+++ spec/displaying_the_best_quote_spec.rb
@@ -0,0 +1,7 @@
+describe 'Displaying the best quote' do
+  describe 'when a best quote is found' do
+    it 'displays the monthly repayment amount to 2 d.p.'
+
+    it 'displays the total replayment amount to 2 d.p.'
+  end
+end
