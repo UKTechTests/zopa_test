1f7c69e23871c5cde0e7a52e06c3f2de9cee59cd
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-10-15 20:13:13 +0100

    Test list - the loan requested should be displayed.

--- spec/displaying_the_best_quote_spec.rb
+++ spec/displaying_the_best_quote_spec.rb
@@ -5,5 +5,7 @@ describe 'Displaying the best quote' do
     it 'displays the total replayment amount to 2 d.p.'
 
     it 'displays the loan rate to 1 d.p.'
+
+    it 'displays the loan requested'
   end
 end
