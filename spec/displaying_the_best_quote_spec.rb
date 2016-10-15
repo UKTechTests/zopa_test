8d3b5559e94c1ba8fb1b17af1e048e67c1a79be9
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-10-15 20:15:37 +0100

    Test list - listed examples for when a different best quote is found.

--- spec/displaying_the_best_quote_spec.rb
+++ spec/displaying_the_best_quote_spec.rb
@@ -8,4 +8,14 @@ describe 'Displaying the best quote' do
 
     it 'displays the loan requested'
   end
+
+  describe 'when a different best quote is found' do
+    it 'displays the monthly repayment amount to 2 d.p.'
+
+    it 'displays the total replayment amount to 2 d.p.'
+
+    it 'displays the loan rate to 1 d.p.'
+
+    it 'displays the loan requested'
+  end
 end
