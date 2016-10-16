0a2038bc3b47d4cdfd46c78467614c19f3e869a5
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-10-16 16:45:36 +0100

    Refactor - removed the quadruplicate market instanciation to a setup helper.

--- spec/finding_the_best_quote_spec.rb
+++ spec/finding_the_best_quote_spec.rb
@@ -20,36 +20,33 @@ describe "Zopa's Lending Market" do
       end
 
       let(:loan) { 1000 }
+      let(:market) do
+        Zopa::Market.new(
+          { 'Lender' => 'Len', 'Rate' => 0.07, 'Available' => 1000 }
+        )
+      end
       
       it 'returns the loan rate offered to 1 d.p.' do
-        best_quote = Zopa::Market.new(
-          { 'Lender' => 'Len', 'Rate' => 0.07, 'Available' => 1000 }
-        ).best_quote(loan)
-        
+        best_quote = market.best_quote loan
+
         expect(best_quote.rate).to eq '7.0%'
       end
 
       it 'returns the loan requested' do
-        best_quote = Zopa::Market.new(
-          { 'Lender' => 'Len', 'Rate' => 0.07, 'Available' => 1000 }
-        ).best_quote(loan)
-        
+        best_quote = market.best_quote loan
+
         expect(best_quote.requested_amount).to eq '£1000'
       end
 
       it 'returns the total repayment to 2 d.p.' do
-        best_quote = Zopa::Market.new(
-          {'Lender' => 'Len', 'Rate' => 0.07, 'Available' => 1000 }
-        ).best_quote(loan)
-        
+        best_quote = market.best_quote loan
+
         expect(best_quote.total_repayment).to eq '£1111.58'
       end
 
       it 'returns the monthly repayment to 2 d.p.' do
-        best_quote = Zopa::Market.new(
-          {'Lender' => 'Len', 'Rate' => 0.07, 'Available' => 1000 }
-        ).best_quote(loan)
-                     
+        best_quote = market.best_quote loan             
+
         expect(best_quote.monthly_repayment).to eq '£30.88'
       end
     end
