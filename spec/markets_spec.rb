1c541028e9d2cbcbdd9887a83551217affb1ca6b
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-10-16 13:45:06 +0100

    Test list - when the market has only one offer/quote, the loan that is being requested is returned.

--- spec/markets_spec.rb
+++ spec/markets_spec.rb
@@ -2,6 +2,8 @@ describe 'Zopa Markets' do
   describe '#best_quote' do
     context 'when the market has only one quote' do
       it 'returns the loan rate offered to 1 d.p.'
+
+      it 'returns the loan requested'
     end
   end
 end
