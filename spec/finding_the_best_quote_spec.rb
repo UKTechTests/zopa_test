10e21c9c1f79edcd714c76b01573aae2f2310faa
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-10-16 21:05:56 +0100

    Added a comment to say that the exact formula for monthly payments (as given in the Wiki) is used.

--- spec/finding_the_best_quote_spec.rb
+++ spec/finding_the_best_quote_spec.rb
@@ -24,6 +24,9 @@ describe "Zopa's Lending Market" do
           payment_period * monthly_payment(quote)
         end
 
+        # The formula for the monthly payment is assumed to the exact one
+        # P = Li/[1 - (1 + i)^-n]
+        # (see https://en.wikipedia.org/wiki/Compound_interest)
         def monthly_payment(quote)
           monthly_interest = quote['Rate']/12
 
