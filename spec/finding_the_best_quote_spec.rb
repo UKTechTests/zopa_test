ebfb0e26a1183b60c9436588b51135724c1925bf
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-10-16 15:36:01 +0100

    Fake it 'til we make it - when the market has only one offer, its rate is returned to 2 d.p.

--- spec/finding_the_best_quote_spec.rb
+++ spec/finding_the_best_quote_spec.rb
@@ -1,7 +1,28 @@
+require 'ostruct'
 describe "Zopa's Lending Market" do
   describe 'finding the best quote' do
     context 'when the market has only one quote' do
-      it 'returns the loan rate offered to 1 d.p.'
+      module Zopa
+        class Market
+          def initialize quote
+
+          end
+
+          def best_quote loan
+            OpenStruct.new(rate: '7.0%')
+          end
+        end
+      end
+      
+      it 'returns the loan rate offered to 1 d.p.' do
+        loan = 1000
+        
+        best_quote = Zopa::Market.new(
+          { 'Lender' => 'Len', 'Rate' => 0.07, 'Available' => 1000 }
+        ).best_quote(loan)
+        
+        expect(best_quote.rate).to eq '7.0%'
+      end
 
       it 'returns the loan requested'
 
