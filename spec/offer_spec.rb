b9a6502793cea22496daac0ea30be0e4bba4d251
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-10-18 20:47:12 +0100

    The Offer returns the amount that may be borrowed to within a reasonable degree of accuracy.

--- spec/offer_spec.rb
+++ spec/offer_spec.rb
@@ -2,12 +2,16 @@ require 'spec_helper'
 require 'offer'
 describe 'An offer' do
   it 'returns the interest rate as a percentage' do
-    offer = Zopa::Market::Offer.new('Available' => 250.0, 'Rate' => 0.032)
+    offer = Zopa::Market::Offer.new('Available' => 15000.0, 'Rate' => 0.032)
     
     expect(offer.interest_rate).to be_within(0.001).of 3.2
   end
 
-  it 'returns the amount that may be borrowed'
+  it 'returns the amount that may be borrowed' do
+    offer = Zopa::Market::Offer.new('Available' => 15000.0, 'Rate' => 0.032)
+    
+    expect(offer.available).to be_within(0.001).of 15000
+  end
 
   it 'returns the monthly repayment'
 
