4ef42d327ae3347d877878b33e1493f5dedeec77
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-10-18 20:51:57 +0100

    The Offer returns the monthly repayment to within a reasonable degree of accuracy.

--- spec/offer_spec.rb
+++ spec/offer_spec.rb
@@ -13,7 +13,11 @@ describe 'An offer' do
     expect(offer.available).to be_within(0.001).of 15000
   end
 
-  it 'returns the monthly repayment'
+  it 'returns the monthly repayment' do
+    offer = Zopa::Market::Offer.new('Available' => 15000.0, 'Rate' => 0.032)
+    
+    expect(offer.monthly_payment(36)).to be_within(0.01).of 437.54
+  end
 
   it 'returns the total repayment'
 end
