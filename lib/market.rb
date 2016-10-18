34555dce1548d717cf51d783dd113d654eafc588
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-10-18 13:23:20 +0100

    Refactor - Offer moved inside Market, as it makes sense in that context.

--- lib/market.rb
+++ lib/market.rb
@@ -32,36 +32,36 @@ module Zopa
           "£#{offer.total_payment(payment_period).round(2)}"
       )
     end
-  end
-
-  class Offer
-    attr_reader :available, :rate
+    
+    class Offer
+      attr_reader :available, :rate
         
-    def initialize offer
-      @rate = offer['Rate']
-      @available = offer['Available']
-    end
+      def initialize offer
+        @rate = offer['Rate']
+        @available = offer['Available']
+      end
 
-    def offering? loan
-      available == loan
-    end
+      def offering? loan
+        available == loan
+      end
 
-    def interest_rate
-      rate * 100
-    end
+      def interest_rate
+        rate * 100
+      end
 
-    def total_payment(payment_period)
-      payment_period * monthly_payment(payment_period)
-    end
+      def total_payment(payment_period)
+        payment_period * monthly_payment(payment_period)
+      end
 
-    # The formula for the monthly payment is assumed to the exact one
-    # P = Li/[1 - (1 + i)^-n]
-    # (see https://en.wikipedia.org/wiki/Compound_interest)
-    def monthly_payment(payment_period)
-      monthly_interest = rate / 12
+      # The formula for the monthly payment is assumed to the exact one
+      # P = Li/[1 - (1 + i)^-n]
+      # (see https://en.wikipedia.org/wiki/Compound_interest)
+      def monthly_payment(payment_period)
+        monthly_interest = rate / 12
 
-      (available * monthly_interest) /
-        (1 - (1 + monthly_interest)**(-payment_period))
+        (available * monthly_interest) /
+          (1 - (1 + monthly_interest)**(-payment_period))
+      end
     end
   end
 end
