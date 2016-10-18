fa3e349ab3a7b03275f76b03dd215e79dd7d0090
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-10-18 20:40:11 +0100

    Created a test list for the Offer object.

---
+++ spec/offer_spec.rb
@@ -0,0 +1,9 @@
+describe 'An offer' do
+  it 'returns the interest rate as a percentage'
+
+  it 'returns the amount that may be borrowed'
+
+  it 'returns the monthly repayment'
+
+  it 'returns the total repayment'
+end
