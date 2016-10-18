dc21d4d50da010e107446fcef819e3bebf42a26f
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-10-18 20:43:21 +0100

    The Offer interest rate is returned as a % to within a reasonable degree of accuracy.

--- spec/offer_spec.rb
+++ spec/offer_spec.rb
@@ -1,5 +1,11 @@
+require 'spec_helper'
+require 'offer'
 describe 'An offer' do
-  it 'returns the interest rate as a percentage'
+  it 'returns the interest rate as a percentage' do
+    offer = Zopa::Market::Offer.new('Available' => 250.0, 'Rate' => 0.032)
+    
+    expect(offer.interest_rate).to be_within(0.001).of 3.2
+  end
 
   it 'returns the amount that may be borrowed'
 
